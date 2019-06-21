7d2ce1e912b64a32eae8bf10ad81911da8e2ec94
Fito Frias <a212203964@unison.mx>
2019-06-20 22:56:15 -0700

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -10,6 +10,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/coca';
 } else {
-    urlDB = 'mongodb+srv://ftx13:l2ykJHC1og9xVA2y@cluster0-msa3r.mongodb.net/Coca'
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
